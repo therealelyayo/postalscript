b584cbac31cf955c6c58b9e11da95514868e60a3
therealelyayo <60529195+therealelyayo@users.noreply.github.com>
2021-08-01 16:51:06 -0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+apt update -y
+apt upgrade -y
+apt install curl nano git apt-transport-https ca-certificates gnupg2 software-properties-common -y
+curl -sSL https://get.docker.com/ | CHANNEL=stable sh
+curl -L https://github.com/docker/compose/releases/download/$(curl -Ls https://www.servercow.de/docker-compose/latest.php)/docker-compose-$(uname -s)-$(uname -m) > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+git clone https://postalserver.io/start/install /opt/postal/install
+sudo ln -s /opt/postal/install/bin/postal /usr/bin/postal
+docker run -d \
+   --name postal-mariadb \
+   -p 127.0.0.1:3306:3306 \
+   --restart always \
+   -e MARIADB_DATABASE=postal \
+   -e MARIADB_ROOT_PASSWORD=postal \
+   mariadb
+docker run -d \
+   --name postal-rabbitmq \
+   -p 127.0.0.1:5672:5672 \
+   --restart always \
+   -e RABBITMQ_DEFAULT_USER=postal \
+   -e RABBITMQ_DEFAULT_PASS=postal \
+   -e RABBITMQ_DEFAULT_VHOST=postal \
+   rabbitmq:3.8
+
+exit
