15331a12a0071b2429bd2841adaa49438c7f23f3
therealelyayo <60529195+therealelyayo@users.noreply.github.com>
2021-08-01 19:22:40 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 apt update -y
 apt upgrade -y
-apt install curl nano git apt-transport-https ca-certificates gnupg2 software-properties-common -y
+apt install curl nano sudo git apt-transport-https ca-certificates gnupg2 software-properties-common -y
 curl -sSL https://get.docker.com/ | CHANNEL=stable sh
 curl -L https://github.com/docker/compose/releases/download/$(curl -Ls https://www.servercow.de/docker-compose/latest.php)/docker-compose-$(uname -s)-$(uname -m) > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
